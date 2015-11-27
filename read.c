f23fd744dffdd2fb38c75553e6bc3d955cbcb4c6
Eliot BOUTHERIN <ebouther@e2r4p3.42.fr>
2015-11-27 17:24:39 +0100

    read.c norme

--- read.c
+++ read.c
@@ -6,11 +6,22 @@
 /*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 13:44:36 by ebouther          #+#    #+#             */
-/*   Updated: 2015/11/27 16:49:55 by ebouther         ###   ########.fr       */
+/*   Updated: 2015/11/27 17:23:46 by ebouther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "read.h"
+
+static int	ft_new_struct(t_map **map)
+{
+	if (!((*map)->nxt_map = (t_map *)malloc(sizeof(**map))))
+		return (-1);
+	(*map) = (*map)->nxt_map;
+	(*map)->nxt_map = NULL;
+	(*map)->x = 0;
+	(*map)->y = 0;
+	return (0);
+}
 
 static int	ft_nb_occur(char *line, int c, int *first_pos)
 {
@@ -36,11 +47,11 @@ static int	ft_nb_occur(char *line, int c, int *first_pos)
 }
 
 static int	ft_fill_struct(t_map **map, int fd)
-{	
+{
 	char	*line;
 	int		last_pos;
 	int		pos;
-	
+
 	pos = 0;
 	while (get_next_line(fd, &line))
 	{
@@ -51,35 +62,16 @@ static int	ft_fill_struct(t_map **map, int fd)
 				(*map)->x = ft_nb_occur(line, '#', &pos);
 			if (ft_nb_occur(line, '#', &pos) > 0)
 				(*map)->y++;
-		
-			ft_putstr("x: ");
-			ft_putnbr((*map)->x);			
-			ft_putstr("last: ");
-			ft_putnbr(last_pos);
-			ft_putstr("pos: ");
-			ft_putnbr(pos);
-			ft_putstr("\n");
-
 			if ((*map)->x == 2 && pos != last_pos)
 				(*map)->x++;
-
-
 		}
-		else
-		{
-			ft_putstr("  ");
-			if (!((*map)->nxt_map = (t_map *)malloc(sizeof(**map))))
-				return (-1);
-			(*map) = (*map)->nxt_map;
-			(*map)->nxt_map = NULL;
-			(*map)->x = 0;
-			(*map)->y = 0;
-		}
+		else if (ft_new_struct(map) == -1)
+			return (-1);
 	}
 	return (0);
 }
 
-t_map	*ft_get_maps(int fd)
+t_map		*ft_get_maps(int fd)
 {
 	t_map	*map;
 	t_map	*beg;
