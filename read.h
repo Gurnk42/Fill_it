fd7136a8733e42b694160172de385bfffe7fc58e
Eliot BOUTHERIN <ebouther@e2r4p3.42.fr>
2015-11-27 17:37:21 +0100

    ebouther : read.h struct now has map content as str

--- read.h
+++ read.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/27 17:17:29 by ebouther          #+#    #+#             */
+/*   Updated: 2015/11/27 17:27:14 by ebouther         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef READ_H
 # define READ_H
 
@@ -8,12 +20,10 @@ typedef struct	s_map
 {
 	int			x;
 	int			y;
-	//char		**map;
+	char		*content;
 	void		*nxt_map;
 }				t_map;
 
-
-t_map	*get_maps(int fd);
-
+t_map	*ft_get_maps(int fd);
 
 #endif
